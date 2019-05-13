d77c0ac8f5c588ce09a61dd57317570ea3182ca2
Daniel <mail@danielluedecke.de>
2019-05-13 22:47:13 +0200

    fix how_to_plot

--- R/how_to_plot.R
+++ R/how_to_plot.R
@@ -6,38 +6,37 @@
 #' @param ... Arguments passed to or from other methods.
 #'
 #' @examples
-#' \dontrun{
 #' library(bayestestR)
 #' results <- hdi(rnorm(1000))
-#'
 #' how_to_plot(results)
-#' }
 #' @export
 how_to_plot  <- function(x, ...) {
   UseMethod("how_to_plot")
-
-  # We'll reactivate this argument if we find a purpose for it :)
-  # if (source != TRUE) {
-  #   warning("Be sure to set 'source = TRUE' to access source code.")
-  # }
 }
 
 
-
-
-
-#' @rdname how_to_plot
 #' @export
 how_to_plot.hdi  <- function(x, ...) {
-  cat(.how_to_plode_cleaner("plot.hdi"))
+  cat(.how_to_plode_cleaner("plot.see_hdi"))
 }
 
-#' @rdname how_to_plot
+
+#' @export
+how_to_plot.ci  <- function(x, ...) {
+  cat(.how_to_plode_cleaner("plot.see_ci"))
+}
+
+
 #' @export
 how_to_plot.p_direction  <- function(x, ...) {
-  cat(.how_to_plode_cleaner("plot.p_direction"))
+  cat(.how_to_plode_cleaner("plot.see_p_direction"))
 }
 
+
+#' @export
+how_to_plot.rope  <- function(x, ...) {
+  cat(.how_to_plode_cleaner("plot.see_rope"))
+}
 
 
 
@@ -46,23 +45,20 @@ how_to_plot.p_direction  <- function(x, ...) {
 #' @importFrom utils head tail getAnywhere
 #' @keywords internal
 .how_to_plode_cleaner <- function(name){
-  sourcecode <- getAnywhere(name)
+  sourcecode <- utils::getAnywhere(name)
   code <- sourcecode$objs[sourcecode$dups == FALSE]
   code <- as.character(code)
   # Split by line
-  code <- strsplit(code, "\n", fixed=TRUE)[[1]]
+  code <- strsplit(code, "\n", fixed = TRUE)[[1]]
 
   # Remove beginning and end of function
-  code <- tail(head(code, -2), -5)
+  code <- utils::tail(utils::head(code, -2), -5)
   # Replace begining
-  code <- gsub("p <- x", "data_plot(x)", code, fixed=TRUE)
+  code <- gsub("p <- x", "data_plot(x)", code, fixed = TRUE)
   # Split
-  code <- gsub("+", "+\n", code, fixed=TRUE)
+  code <- gsub("+", "+\n ", code, fixed = TRUE)
+  code <- gsub("%>%", "%>%\n ", code, fixed = TRUE)
 
-  code <- paste0("Assuming that the input object is `x`:",
-                 "\n\n",
-                 code)
-  return(code)
-
+  paste0("Assuming that the input object is `x`:\n\n", trimws(code))
 }
 
