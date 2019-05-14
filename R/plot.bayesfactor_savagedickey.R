cd96c6b925c46061a8fa41678a00c1ee62925f38
Daniel <mail@danielluedecke.de>
2019-05-14 21:05:58 +0200

    use .data

--- R/plot.bayesfactor_savagedickey.R
+++ R/plot.bayesfactor_savagedickey.R
@@ -1,13 +1,14 @@
+#' @importFrom rlang .data
 #' @export
 plot.see_bayesfactor_savagedickey <- function(x, ...) {
   plot_data <- attr(x, "plot_data")
   hypothesis <- attr(x, "hypothesis")
 
-  ggplot(plot_data, aes(x, y, color = Distribution, fill = Distribution)) +
+  ggplot(plot_data, aes(x = .data$x, y = .data$y, color = .data$Distribution, fill = .data$Distribution)) +
     geom_line(size = 1) +
     geom_area(alpha = 0.15) +
     geom_vline(xintercept = hypothesis, linetype = "dashed") +
-    facet_wrap( ~ ind, scales = "free") +
+    facet_wrap(~ind, scales = "free") +
     labs(y = "Density") +
     theme(legend.position = "bottom")
 }
