bab97c0b2382d0b0f4177efbaef9f43549cd1654
mattansb <35330040+mattansb@users.noreply.github.com>
2019-05-14 19:32:22 +0300

    minor

--- R/plot.bayesfactor_savagedickey.R
+++ R/plot.bayesfactor_savagedickey.R
@@ -3,7 +3,7 @@ plot.see_bayesfactor_savagedickey <- function(x, ...) {
   plot_data <- attr(x, "plot_data")
   hypothesis <- attr(x, "hypothesis")
 
-  ggplot(plot_data, aes(x, y, color = Type, fill = Type)) +
+  ggplot(plot_data, aes(x, y, color = Distribution, fill = Distribution)) +
     geom_line(size = 1) +
     geom_area(alpha = 0.15) +
     geom_vline(xintercept = hypothesis, linetype = "dashed") +
