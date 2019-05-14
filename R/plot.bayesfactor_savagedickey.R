fa9873b932fa134c44ec31b3edee70db1557048b
mattansb <35330040+mattansb@users.noreply.github.com>
2019-05-14 19:27:15 +0300

    Create plot.bayesfactor_savagedickey.R

---
+++ R/plot.bayesfactor_savagedickey.R
@@ -0,0 +1,13 @@
+#' @export
+plot.see_bayesfactor_savagedickey <- function(x, ...) {
+  plot_data <- attr(x, "plot_data")
+  hypothesis <- attr(x, "hypothesis")
+
+  ggplot(plot_data, aes(x, y, color = Type, fill = Type)) +
+    geom_line(size = 1) +
+    geom_area(alpha = 0.15) +
+    geom_vline(xintercept = hypothesis, linetype = "dashed") +
+    facet_wrap( ~ ind, scales = "free") +
+    labs(y = "Density") +
+    theme(legend.position = "bottom")
+}
