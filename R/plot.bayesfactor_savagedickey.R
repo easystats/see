ce5a75c0d0686eb7afd2e83dde12cf5a2b12c9cf
Daniel Lüdecke <mail@danielluedecke.de>
2019-05-17 11:32:27 +0200

    make sure point outline matches theme

--- R/plot.bayesfactor_savagedickey.R
+++ R/plot.bayesfactor_savagedickey.R
@@ -12,6 +12,13 @@ plot.see_bayesfactor_savagedickey <- function(x, point_size = 2, ...) {
                      x = .data$x[which.min(abs(.data$x))]) %>%
     dplyr::ungroup()
 
+  # make sure point outline matches theme
+  t <- theme_get()
+  if (is.null(t$panel.grid.major$colour))
+    null_point_outline <- "white"
+  else
+    null_point_outline <- t$panel.grid.major$colour
+
   plot_data %>%
     ggplot(aes(
       x = .data$x,
@@ -22,7 +29,7 @@ plot.see_bayesfactor_savagedickey <- function(x, point_size = 2, ...) {
     geom_line(size = 1) +
     geom_area(alpha = 0.15) +
     geom_vline(xintercept = hypothesis, linetype = "dashed") +
-    geom_point(data = d_points, size = point_size, pch = 21, colour = "white") +
+    geom_point(data = d_points, size = point_size, pch = 21, colour = null_point_outline) +
     facet_wrap(~ind, scales = "free") +
     labs(y = "Density",
          color = "Distribution",
