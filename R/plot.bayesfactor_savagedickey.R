24d7bee97e4773483f66ff48ae7d68b23112a81e
Daniel Lüdecke <mail@danielluedecke.de>
2019-05-17 11:20:58 +0200

    flexible null-point-size

--- R/plot.bayesfactor_savagedickey.R
+++ R/plot.bayesfactor_savagedickey.R
@@ -1,6 +1,8 @@
+#' @rdname data_plot
+#' @param point_size Size of point-geoms.
 #' @importFrom rlang .data
 #' @export
-plot.see_bayesfactor_savagedickey <- function(x, ...) {
+plot.see_bayesfactor_savagedickey <- function(x, point_size = 2, ...) {
   plot_data <- attr(x, "plot_data")
   hypothesis <- attr(x, "hypothesis")
 
@@ -20,7 +22,7 @@ plot.see_bayesfactor_savagedickey <- function(x, ...) {
     geom_line(size = 1) +
     geom_area(alpha = 0.15) +
     geom_vline(xintercept = hypothesis, linetype = "dashed") +
-    geom_point(data = d_points) +
+    geom_point(data = d_points, size = point_size) +
     facet_wrap(~ind, scales = "free") +
     labs(y = "Density",
          color = "Distribution",
