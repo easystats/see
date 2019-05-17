773e48fb8b8508dc08808c42a667b8f6ee63d0b4
mattansb <35330040+mattansb@users.noreply.github.com>
2019-05-17 12:27:57 +0300

    within -> mutate
    also made code more readable
    #23

--- R/plot.bayesfactor_models.R
+++ R/plot.bayesfactor_models.R
@@ -3,69 +3,70 @@
 #' @param value What value to display.
 #' @param log Show log-transformed Bayes factors.
 #' @export
-plot.see_bayesfactor_models <- function(x, n_pies = c("one", "many"), value = c("none", "BF", "probability"), log = FALSE, ...) {
+plot.see_bayesfactor_models <-
+  function(x,
+           n_pies = c("one", "many"),
+           value = c("none", "BF", "probability"),
+           log = FALSE,
+           ...) {
+
   n_pies <- match.arg(n_pies)
   value <- match.arg(value)
 
-  plot_data <- as.data.frame(x) %>%
-    within({
-      Model <- factor(Model, levels = Model)
-      PostProb <- BF / sum(BF)
-      pos_txt <- sum(PostProb) + PostProb / 2 - cumsum(PostProb)
-    })
+  denominator <- attr(x, "denominator")
+  denominator_name <- x$Model[denominator]
 
-  if (log) {
-    plot_data$BF <- log(plot_data$BF)
+  # Prep data and bar position:
+  one_pie_data <- as.data.frame(x) %>%
+    dplyr::ungroup() %>%
+    dplyr::mutate(
+      PostProb = .data$BF / sum(.data$BF),
+      pos_txt  = sum(.data$PostProb) + .data$PostProb / 2 - cumsum(.data$PostProb),
+      Model    = factor(.data$Model, levels = unique(.data$Model))
+    )
+
+  many_pies_data <- suppressWarnings(
+    rbind(one_pie_data %>%
+            dplyr::mutate(Type     = denominator_name,
+                          BF       = .data$BF[denominator],
+                          PostProb = .data$PostProb[denominator]),
+          one_pie_data %>%
+            dplyr::mutate(Type = .data$Model)) %>%
+      dplyr::group_by(.data$Model) %>%
+      dplyr::mutate(pos_bar = .data$PostProb/sum(.data$PostProb),
+                    pos_txt = sum(.data$pos_bar) + .data$pos_bar / 2 - cumsum(.data$pos_bar)) %>%
+      dplyr::filter(.data$Model != denominator_name) %>%
+      dplyr::ungroup() %>%
+      dplyr::mutate(Type = factor(.data$Type, levels = unique(.data$Type)))
+  )
+
+
+  if (value == "BF") {
+    if (log) {
+      one_pie_data$label <- round(log(one_pie_data$BF), 2)
+      many_pies_data$label <- round(log(many_pies_data$BF), 2)
+    } else {
+      one_pie_data$label <- round(one_pie_data$BF, 2)
+      many_pies_data$label <- round(many_pies_data$BF, 2)
+    }
+  } else if (value == "probability") {
+    one_pie_data$label <- paste0(round(one_pie_data$PostProb * 100, 1),"%")
+    many_pies_data$label <- paste0(round(many_pies_data$PostProb * 100, 1),"%")
+  } else {
+    one_pie_data$label <- ""
+    many_pies_data$label <- ""
   }
 
+
   if (n_pies == "one") {
-    if (value == "BF") {
-      plot_data$label <- round(plot_data$BF, 2)
-    } else if (value == "probability") {
-      plot_data$label <- round(plot_data$PostProb * 100, 1)
-    } else {
-      plot_data$label <- ""
-    }
-
-    plot_data$Model <- factor(plot_data$Model, levels = unique(plot_data$Model))
-
-    ggplot(plot_data, aes(x = "", y = .data$PostProb, fill = .data$Model)) +
+    ggplot(one_pie_data, aes(x = "", y = .data$PostProb, fill = .data$Model)) +
       geom_bar(width = 1, stat = "identity", color = "white", size = .5) +
       geom_text(aes(y = .data$pos_txt, label = .data$label), position = position_nudge(.1)) +
       coord_polar("y", start = 0) +
       scale_y_continuous(expand = c(0, 0)) +
       labs(x = "", y = "", fill = "Model")
   } else {
-    denominator <- attr(x, "denominator")
-    denominator_name <- x$Model[denominator]
-
-    plot_data2 <- plot_data %>%
-      within({
-        Type <- denominator_name
-        BF <- BF[denominator]
-        PostProb <- PostProb[denominator]
-      }) %>%
-      rbind(within(plot_data,Type <- Model)) %>%
-      split(.$Model) %>%
-      lapply(function(.x) {
-        .x$pos_bar <- .x$PostProb / sum(.x$PostProb)
-        .x$pos_txt <- sum(.x$pos_bar) + .x$pos_bar / 2 - cumsum(.x$pos_bar)
-        .x
-      }) %>%
-      do.call("rbind", .)
-
-    plot_data2 <- plot_data2[plot_data2$Model != denominator_name, ]
-    plot_data2$Type <- factor(plot_data2$Type, levels = unique(plot_data2$Type))
-
-    if (value == "BF") {
-      plot_data2$label <- round(plot_data2$BF,2)
-    } else if (value == "probability") {
-      plot_data2$label <- round(plot_data2$PostProb*100,1)
-    } else {
-      plot_data2$label <- ""
-    }
-
-    ggplot(plot_data2, aes(x = "", y = .data$pos_bar, fill = .data$Type)) +
+    ggplot(many_pies_data, aes(x = "", y = .data$pos_bar, fill = .data$Type)) +
       geom_bar(width = 1, stat = "identity", color = "white", size = .5) +
       geom_text(aes(y = .data$pos_txt, label = .data$label), position = position_nudge(.1)) +
       coord_polar("y", start = 0) +
