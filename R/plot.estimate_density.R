3287f190dd49e398769f43823f627920e6f5b37e
Daniel <mail@danielluedecke.de>
2019-05-20 12:05:34 +0200

    add docs, add show_intercept

--- R/plot.estimate_density.R
+++ R/plot.estimate_density.R
@@ -22,11 +22,23 @@ data_plot.estimate_density <- function(x, ...){
 
 
 # Plot --------------------------------------------------------------------
+#' @rdname data_plot
+#' @param stack Logical, if \code{TRUE}, densities are plotted as stacked lines.
+#'   Else, densities are plotted for each parameter among each other.
+#' @param show_intercept Logical, if \code{TRUE}, the intercept-parameter is included
+#'   in the plot. By default, it is hidden because in many cases the intercet-parameter
+#'   has a very different density, so density curves for other parameters are
+#'   hardly visible.
 #' @importFrom rlang .data
 #' @export
-plot.see_estimate_density <- function(x, stack = TRUE, ...){
+plot.see_estimate_density <- function(x, stack = TRUE, show_intercept = FALSE, ...){
   if (!"data_plot" %in% class(x)) {
     x <- data_plot(x, ...)
+  }
+
+  if (!show_intercept) {
+    remove <- which(x$Parameter %in% c("Intercept", "(Intercept)"))
+    if (length(remove)) x <- x[-remove, ]
   }
 
   if (stack == TRUE) {
