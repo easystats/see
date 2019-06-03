fd982516be650ca1863226c5d255393cbc33c754
Daniel <mail@danielluedecke.de>
2019-06-03 19:10:47 +0200

    add further diagnostic-plots

--- R/print.check_model.R
+++ R/print.check_model.R
@@ -8,6 +8,8 @@ print.see_check_model <- function(x, ...) {
   if ("VIF" %in% names(x)) p$VIF <- .plot_diag_vif(x$VIF)
   if ("QQ" %in% names(x)) p$QQ <- .plot_diag_qq(x$QQ)
   if ("NORM" %in% names(x)) p$NORM <- .plot_diag_norm(x$NORM)
+  if ("NCV" %in% names(x)) p$NCV <- .plot_diag_ncv(x$NCV)
+  if ("HOMOGENEITY" %in% names(x)) p$HOMOGENEITY <- .plot_diag_homogeneity(x$HOMOGENEITY)
 
   if (panel) {
     do.call(plots, p)
@@ -53,7 +55,7 @@ print.see_check_model <- function(x, ...) {
 
 
 .plot_diag_qq <- function(x) {
-  ggplot(x, aes(x = .data$x, .data$y)) +
+  ggplot(x, aes(x = .data$x, y = .data$y)) +
     geom_point2(colour = unname(flat_colors("blue grey"))) +
     stat_smooth(method = "lm", se = FALSE, size = .9, colour = unname(flat_colors("blue"))) +
     labs(
@@ -63,4 +65,55 @@ print.see_check_model <- function(x, ...) {
       x = "Theoretical quantiles (predicted values)"
     ) +
     theme_bw()
+}
+
+
+
+
+.plot_diag_homogeneity <- function(x) {
+  ggplot(x, aes(x = .data$x, .data$y)) +
+    geom_point2(colour = unname(flat_colors("blue grey"))) +
+    stat_smooth(method = "loess", se = FALSE, size = .9, colour = unname(flat_colors("blue"))) +
+    labs(
+      title = "Homogeneity of Variance (Scale-Location)",
+      subtitle = "Dots should spread equally around a horizontal line",
+      y = "Std. Residuals (sqrt)",
+      x = "Fitted values"
+    ) +
+    theme_bw()
+}
+
+
+
+.plot_diag_ncv <- function(x) {
+  ggplot(x, aes(x = .data$x, y = .data$y)) +
+    geom_point2(colour = unname(flat_colors("blue grey"))) +
+    geom_smooth(method = "loess", se = FALSE, size = .9, colour = unname(flat_colors("blue"))) +
+    labs(
+      x = "Fitted values",
+      y = "Residuals",
+      title = "Homoscedasticity (Linear Relationship)",
+      subtitle = "Dots should show no distinct patterns around a horizontal line"
+    ) +
+    theme_bw()
+}
+
+
+
+.plot_diag_reqq <- function(x) {
+  ggplot(x, aes_string(
+    x = "nQQ",
+    y = "y"
+  )) +
+    facet_wrap(~ ind, scales = "free") +
+    labs(x = "Standard normal quantiles", y = "Random effect quantiles") +
+    geom_intercept_line2(0, NULL) +
+    stat_smooth(method = "lm", alpha = alpha) +
+    geom_errorbar(
+      aes_string(ymin = "conf.low", ymax = "conf.high"),
+      width = 0,
+      colour = "black"
+    ) +
+    geom_point(size = dot.size, colour = "darkblue")
+}
 }
