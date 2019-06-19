d57ba55131db7dd46097cdd0f4dbdc28fab73347
Daniel <mail@danielluedecke.de>
2019-06-19 22:13:03 +0200

    revdep

--- revdep/check.R
+++ revdep/check.R
@@ -1,5 +1,6 @@
 tools::check_packages_in_dir(
-  dir = "C:/Users/mail/Documents/R/sj",
+  # dir = "C:/Users/mail/Documents/R/sj",
+  dir = "D:/mail/Documents/Coding/R/easystats/",
   check_args = c("--as-cran", ""),
   reverse = list(repos = getOption("repos")["CRAN"])
 )
