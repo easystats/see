847d2a7e0fef1e735a6465148325ca00bd634087
Dominique Makowski <dom.mak19@gmail.com>
2019-10-02 12:08:58 +0800

    Took the middle path, a bar chart instead of lolipops and area
    Makes everyone happy I hope hehe

--- R/plot.n_factors.R
+++ R/plot.n_factors.R
@@ -1,34 +1,39 @@
 #' @export
-data_plot.n_factors <- function(x, data = NULL, type = "line", ...){
+data_plot.n_factors <- function(x, data = NULL, ...){
+
   s1 <- summary(x)
 
-  if (type == "line") {
-    s1$n_Factors <- as.factor(s1$n_Factors)
-    s2 <- data.frame(n_Factors = factor(1:max(x$n_Factors)), n_Methods = 0)
-  } else {
-    s2 <- data.frame(n_Factors = 1:max(x$n_Factors), n_Methods = 0)
+  if("n_Factors" %in% names(x)){
+    var <- "n_Factors"
+    lab <- "factors"
+  } else{
+    var <- "n_Clusters"
+    lab <- "clusters"
   }
 
-  dataplot <- rbind(s1, s2[!s2$n_Factors %in% s1$n_Factors, ])
+  s2 <- data.frame(n_Factors = 1:max(x[[var]]), n_Methods = 0)
+  dataplot <- rbind(s1, s2[!s2[[var]] %in% s1[[var]], ])
 
-  if (type == "line") {
-    dataplot$x <- factor(dataplot$n_Factors, levels = rev(sort(levels(dataplot$n_Factors))))
-    dataplot$group <- "0"
-    dataplot$group[which.max(dataplot$n_Methods)] <- "1"
-  } else {
-    dataplot$x <- dataplot$n_Factors
-  }
+  # Format and reorder
+  rownames(dataplot) <- NULL
+  dataplot <- dataplot[order(dataplot$n_Factors), ]
 
+  # Add aes
+  dataplot$x <- dataplot[[var]]
   dataplot$y <- dataplot$n_Methods / sum(dataplot$n_Methods)
-  attr(dataplot, "info") <- list("xlab" = "Number of Factors",
+  dataplot$fill <- "Not-optimal"
+  dataplot$fill[which.max(dataplot$n_Methods)] <- "Optimal"
+
+  attr(dataplot, "info") <- list("xlab" = paste("Number of", lab),
                                  "ylab" = "Agreement between methods",
-                                 "title" = "How many factors to retain")
+                                 "title" = paste("How many", lab, "to retain"))
 
   class(dataplot) <- unique(c("data_plot", "see_n_factors", class(dataplot)))
   dataplot
 }
 
-
+#' @export
+data_plot.n_clusters <- data_plot.n_factors
 
 
 
@@ -36,29 +41,20 @@ data_plot.n_factors <- function(x, data = NULL, type = "line", ...){
 #' @importFrom rlang .data
 #' @rdname data_plot
 #' @export
-plot.see_n_factors <- function(x, data = NULL, type = c("line", "area"), ...) {
-  type <- match.arg(type)
+plot.see_n_factors <- function(x, data = NULL, ...) {
+
   if (!"data_plot" %in% class(x)) {
-    x <- data_plot(x, data = data, type = type)
+    x <- data_plot(x, data = data)
   }
 
-  if (type == "area") {
-    ggplot(x, aes(x = .data$x, y = .data$y)) +
-      geom_area(fill = "#2196F3") +
-      geom_segment(aes(x = .data$x[which.max(.data$y)], xend = .data$x[which.max(.data$y)], y = 0, yend = max(.data$y)), color = "#E91E63", linetype = "dashed") +
-      geom_point(aes(x = .data$x[which.max(.data$y)], y = max(.data$y)), color = "#E91E63") +
-      scale_color_manual(values = c("black", "#E91E63")) +
-      scale_y_continuous(labels = scales::percent) +
-      scale_x_continuous(breaks = 1:max(x$x)) +
-      add_plot_attributes(x)
-  } else {
-    ggplot(x, aes(x = .data$x, y = .data$y, colour = .data$group)) +
-      geom_segment(aes(y = 0, xend = .data$x, yend = .data$y)) +
-      geom_point() +
-      coord_flip() +
-      guides(colour = FALSE) +
-      scale_y_continuous(labels = scales::percent) +
-      add_plot_attributes(x)
-  }
+  ggplot(x, aes(x = .data$x, y = .data$y, fill = .data$fill)) +
+    geom_bar(stat="identity") +
+    guides(fill = FALSE) +
+    scale_y_continuous(labels = scales::percent) +
+    add_plot_attributes(x) +
+    scale_x_continuous(breaks = 1:max(x$x)) +
+    scale_fill_manual(values = c("black", "red"))
 }
 
+#' @export
+plot.see_n_clusters <- plot.see_n_factors
