fefbe68c10c42f3a389ca45d398d95d418f9c0c2
Daniel <mail@danielluedecke.de>
2019-11-01 08:56:57 +0100

    ordeeeerrr!

--- R/plot.parameters_pca.R
+++ R/plot.parameters_pca.R
@@ -1,13 +1,12 @@
 #' @importFrom dplyr group_by mutate ungroup select one_of n
 #' @export
 data_plot.parameters_pca <- function(x, data = NULL, ...){
-  x$Variable <- factor(x$Variable, levels = unique(x$Variable))
-
   dataplot <- as.data.frame(x)
   dataplot$Complexity <- NULL
   dataplot$Uniqueness <- NULL
 
   dataplot <- .reshape_to_long(dataplot, names_to = "Component", values_to = "y", columns = 2:ncol(dataplot))
+  dataplot$Variable <- factor(dataplot$Variable, levels = rev(unique(dataplot$Variable)))
 
   rotation_name <- attr(x, "rotation", exact = TRUE)
 
@@ -74,13 +73,13 @@ plot.see_parameters_pca <- function(x, type = c("bar", "line"), text_size = 3.5,
   }
 
   p <- p +
-    geom_text(aes(y = -.15, label = round(.data$y, 2)), color = text_color, size = text_size) +
+    geom_text(aes(y = abs(.data$y), label = round(.data$y, 2)), color = text_color, size = text_size, nudge_y = .15) +
     coord_flip() +
     guides(fill = FALSE, color = FALSE) +
     scale_y_continuous(
-      limits = c(-.25, 1),
-      breaks = c(-.25, 0, .25, .5, .75, 1),
-      labels = c("", "0", "0.25", "0.5", "0.75", "1")
+      limits = c(0, 1.25),
+      breaks = c(0, .25, .5, .75, 1, 1.25),
+      labels = c("0", "0.25", "0.5", "0.75", "1", "")
     ) +
     facet_wrap(~ Component) +
     add_plot_attributes(x)
