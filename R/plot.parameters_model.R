ed3f2d9cfa5bc4fb45c971b041e93f11c0f78924
Daniel Lüdecke <mail@danielluedecke.de>
2019-11-14 09:46:53 +0100

    close #55

--- R/plot.parameters_model.R
+++ R/plot.parameters_model.R
@@ -7,8 +7,11 @@ plot.see_parameters_model <- function(x, show_intercept = FALSE, point_size = .8
     colnames(x)[which.min(match(colnames(x), c("Median", "Mean", "Map")))] <- "Coefficient"
   }
 
-  # remember components
+  # is exp?
+  exponentiated_coefs <- isTRUE(attributes(x)$exponentiate)
+  y_intercept <- ifelse(exponentiated_coefs, 1, 0)
 
+  # remember components
   has_effects <- "Effects" %in% colnames(x) && length(unique(x$Effects)) > 1
   has_component <- "Component" %in% colnames(x) && length(unique(x$Component)) > 1
   has_response <- "Response" %in% colnames(x) && length(unique(x$Response)) > 1
@@ -19,16 +22,12 @@ plot.see_parameters_model <- function(x, show_intercept = FALSE, point_size = .8
   cp <- attributes(x)$cleaned_parameters
   is_meta <- !is.null(mc) && mc %in% c("rma", "rma.uni")
 
-
   # minor fixes for Bayesian models
-
   if (!is.null(mc) && !is.null(cp) && mc %in% c("stanreg", "stanmvreg", "brmsfit")) {
     x$Parameter <- cp
   }
 
-
   # data preparation for metafor-objects
-
   if (is_meta) {
     overall <- which(x$Parameter == "(Intercept)")
     x$Parameter[overall] <- "Overall"
@@ -40,11 +39,9 @@ plot.see_parameters_model <- function(x, show_intercept = FALSE, point_size = .8
     x$shape[overall] <- 18
   }
 
-
   # if we have a model with multiple responses or response levels
   # remove name of level from parameter name, as we split the plot
   # by response level anyway...
-
   if (has_response) {
     for (i in rev(sort(unique(x$Response)))) {
       x$Parameter <- gsub(i, "", x$Parameter)
@@ -52,9 +49,7 @@ plot.see_parameters_model <- function(x, show_intercept = FALSE, point_size = .8
     x$Parameter <- gsub("^\\:(.*)", "\\1", x$Parameter)
   }
 
-
   # make sure components are sorted in original order, not alphabetically
-
   if (has_effects) {
     x$Effects <- factor(x$Effects, levels = unique(x$Effects))
   }
@@ -77,27 +72,21 @@ plot.see_parameters_model <- function(x, show_intercept = FALSE, point_size = .8
     x$Parameter <- factor(x$Parameter, levels = rev(unique(x$Parameter)))
   }
 
-
   if (is_meta) {
-
     # plot setup for metafor-objects
-
     p <- ggplot(x, aes(x = .data$Parameter, y = .data$Coefficient, color = .data$group)) +
-      geom_hline(aes(yintercept = 0), linetype = "dotted") +
+      geom_hline(aes(yintercept = y_intercept), linetype = "dotted") +
       geom_pointrange(aes(ymin = .data$CI_low, ymax = .data$CI_high), size = point_size, fatten = x$point_size, shape = x$shape) +
       coord_flip() +
       theme_modern(legend.position = "none") +
       scale_color_material() +
       guides(color = FALSE, size = FALSE, shape = FALSE)
-  }
-  else if (sum(grepl("^CI_low", colnames(x))) > 1) {
-
+  } else if (sum(grepl("^CI_low", colnames(x))) > 1) {
     # plot setup for model parameters with multiple CIs
-
     x <- bayestestR::reshape_ci(x)
     x$CI <- as.character(x$CI)
     p <- ggplot(x, aes(x = .data$Parameter, y = .data$Coefficient, color = .data$CI)) +
-      geom_hline(aes(yintercept = 0), linetype = "dotted") +
+      geom_hline(aes(yintercept = y_intercept), linetype = "dotted") +
       geom_pointrange(
         aes(ymin = .data$CI_low, ymax = .data$CI_high),
         size = point_size,
@@ -107,12 +96,10 @@ plot.see_parameters_model <- function(x, show_intercept = FALSE, point_size = .8
       theme_modern() +
       scale_color_material()
   } else {
-
     # plot setup for regular model parameters
-
     x$group <- as.factor(x$Coefficient < 0)
     p <- ggplot(x, aes(x = .data$Parameter, y = .data$Coefficient, color = .data$group)) +
-      geom_hline(aes(yintercept = 0), linetype = "dotted") +
+      geom_hline(aes(yintercept = y_intercept), linetype = "dotted") +
       geom_pointrange(aes(ymin = .data$CI_low, ymax = .data$CI_high), size = point_size) +
       coord_flip() +
       theme_modern(legend.position = "none") +
@@ -120,8 +107,23 @@ plot.see_parameters_model <- function(x, show_intercept = FALSE, point_size = .8
   }
 
 
-  # wrap plot into facets, depending on the components
+  # check for exponentiated estimates. in such cases, we transform the y-axis
+  # to log-scale, to get proper proportion of exponentiated estimates. to
+  # do this, we create a pretty range of values, and then look for lowest and
+  # largest data points that are within this range. Thereby we have the pretty
+  # values we can use as breaks and labels for the scale...
+  if (exponentiated_coefs) {
+    range <- 2^c(-24:16)
+    x_low <- which.min(min(x$CI_low) > range) - 1
+    x_high <- which.max(max(x$CI_high) < range)
+    p <- p + scale_y_log10(
+      breaks = range[x_low:x_high],
+      limits = c(range[x_low], range[x_high]),
+      labels = sprintf("%g", range[x_low:x_high])
+    )
+  }
 
+  # wrap plot into facets, depending on the components
   if (is.null(n_columns)) n_columns <- ifelse(sum(has_component, has_response, has_effects) > 1, 2, 1)
 
   if (has_component && has_response && has_effects) {
@@ -140,5 +142,9 @@ plot.see_parameters_model <- function(x, show_intercept = FALSE, point_size = .8
     p <- p + facet_wrap(~Response, ncol = n_columns, scales = "free")
   }
 
-  p + labs(x = "Estimate", y = "Parameter", colour = "CI")
+  p + labs(
+    x = "Parameter",
+    y = ifelse(exponentiated_coefs, "Exp(Estimate)", "Estimate"),
+    colour = "CI"
+  )
 }
