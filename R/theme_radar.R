f7a3132c9b6d269d1be627e033294f9b710e7a6e
Daniel <mail@danielluedecke.de>
2019-11-21 20:18:00 +0100

    update website

--- R/theme_radar.R
+++ R/theme_radar.R
@@ -1,8 +1,10 @@
-#' A lucid theme for radar plots
+#' Themes for radar plots
 #'
-#' A light, clear theme for ggplot.
+#' \code{theme_radar()} is a light, clear theme for ggplot radar-plots, while \code{theme_radar_dark()} is a dark variant of \code{theme_radar()}.
 #'
 #' @inheritParams theme_modern
+#'
+#' @seealso \code{\link{coord_radar}}
 #'
 #' @examples
 #' library(ggplot2)
@@ -34,7 +36,7 @@ theme_radar <- function(
   plot.title.face = "plain",
   plot.title.space = 15,
   legend.position = "right",
-  axis.title.space = 10,
+  axis.title.space = 15,
   legend.title.size = 11,
   legend.text.size = 10,
   axis.title.size = 11,
@@ -71,6 +73,56 @@ theme_radar <- function(
       legend.text      = element_text(colour = "grey25"),
       panel.grid.major = element_line(colour = "grey80"),
       panel.grid.minor = element_line(colour = "grey90"),
+      panel.border     = element_blank()
+    )
+}
+
+
+#' @rdname theme_radar
+#' @export
+theme_radar_dark <- function(
+  base_size = 11,
+  base_family = "",
+  plot.title.size = 12,
+  plot.title.face = "plain",
+  plot.title.space = 15,
+  legend.position = "right",
+  axis.title.space = 15,
+  legend.title.size = 11,
+  legend.text.size = 10,
+  axis.title.size = 11,
+  axis.title.face = "plain",
+  axis.text.size = 10,
+  axis.text.angle = NULL,
+  tags.size = 11,
+  tags.face = "plain") {
+
+  theme_modern(
+    base_size = base_size,
+    base_family = base_family,
+    plot.title.size = plot.title.size,
+    plot.title.face = plot.title.face,
+    plot.title.space = plot.title.space,
+    legend.position = legend.position,
+    axis.title.space = axis.title.space,
+    legend.title.size = legend.title.size,
+    legend.text.size = legend.text.size,
+    axis.title.size = axis.title.size,
+    axis.title.face = axis.title.face,
+    axis.text.size = axis.text.size,
+    axis.text.angle = axis.text.angle,
+    tags.size = tags.size,
+    tags.face = tags.face
+  ) +
+    theme(
+      plot.background = element_rect(fill = "#001429"),
+      panel.background = element_rect(fill = "#001429"),
+      legend.background = element_rect(fill = "#001429"),
+      axis.line = element_blank(),
+      text = element_text(color = "#f2f2f2"),
+      axis.text = element_text(color = "#f2f2f2"),
+      panel.grid.major = element_line(color = "#465463"),
+      strip.text = element_text(color = "#f2f2f2"),
       panel.border     = element_blank()
     )
 }
