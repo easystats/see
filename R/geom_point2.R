7c5a61651c96f1085118fd4c008d788eeaa4234c
Brenton M. Wiernik <bwiernik@users.noreply.github.com>
2020-09-07 12:21:11 -0400

    Add geom_pointrange2 and geom_count2
    To round out the four point-having geoms in ggplot2

--- R/geom_point2.R
+++ R/geom_point2.R
@@ -5,7 +5,7 @@
 #' @param size Size of points.
 #' @param stroke Stroke thickness.
 #' @param shape Shape of points.
-#' @param ... Other arguments to be passed to \code{\link[ggplot2::geom_point]{ggplot2::geom_point}} or \code{\link[ggplot2::geom_jitter]{ggplot2::geom_jitter}}.
+#' @param ... Other arguments to be passed to \code{\link[ggplot2::geom_point]{ggplot2::geom_point}}, \code{\link[ggplot2::geom_jitter]{ggplot2::geom_jitter}}, \code{\link[ggplot2::geom_pointrange]{ggplot2::geom_pointrange}}, or \code{\link[ggplot2::geom_count]{ggplot2::geom_count}}.
 #'
 #' @examples
 #' library(ggplot2)
@@ -40,3 +40,25 @@ geom_jitter2 <- function(..., size = 2, stroke = 0, shape = 16){
 #' @rdname geom_point2
 #' @export
 geom_jitter_borderless <- geom_jitter2
+
+
+#' @rdname geom_point2
+#' @export
+geom_pointrange2 <- function(..., stroke = 0){
+  geom_pointrange(stroke = stroke, ...)
+}
+
+#' @rdname geom_point2
+#' @export
+geom_pointrange_borderless <- geom_pointrange2
+
+
+#' @rdname geom_point2
+#' @export
+geom_count2 <- function(..., stroke = 0){
+  geom_count(stroke = stroke, ...)
+}
+
+#' @rdname geom_point2
+#' @export
+geom_count_borderless <- geom_count2
