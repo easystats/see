59bbecaa4b5449d76de9cf30bcdedef3c7e175ff
Indrajeet Patil <inderonline1988@gmail.com>
2021-02-21 10:36:53 +0100

    get started

---
+++ tests/testthat/test-vdiffr.R
@@ -0,0 +1,20 @@
+if (getRversion() < "4.1" && require("vdiffr") && require("ggplot2")) {
+  test_that("`coord_radar()` works - vdiffr", {
+    skip_on_cran()
+
+    data <- iris %>%
+      dplyr::group_by(Species) %>%
+      dplyr::summarise_all(mean) %>%
+      tidyr::pivot_longer(-Species)
+
+    vdiffr::expect_doppelganger(
+      title = "coord_radar()` works",
+      fig = data %>% ggplot(aes(
+        x = name, y = value, color = Species,
+        group = Species
+      )) +
+        geom_polygon(fill = NA, size = 2) +
+        coord_radar(start = -pi / 4)
+    )
+  })
+}
