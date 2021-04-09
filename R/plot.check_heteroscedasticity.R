306fe0bb8b48d217d40726de0e9b47f3c2b35569
Daniel <mail@danielluedecke.de>
2021-04-09 11:49:25 +0200

    #106

--- R/plot.check_heteroscedasticity.R
+++ R/plot.check_heteroscedasticity.R
@@ -13,7 +13,8 @@
 #' result <- check_heteroscedasticity(m)
 #' result
 #' plot(result, data = m) # data required for pkgdown
-#' @importFrom stats residuals sd dnorm
+#' @importFrom stats residuals rstandard fitted
+#' @importFrom insight get_variance_residual model_info
 #' @export
 plot.see_check_heteroscedasticity <- function(x, data = NULL, ...) {
   if (is.null(data)) {
@@ -21,9 +22,51 @@ plot.see_check_heteroscedasticity <- function(x, data = NULL, ...) {
   } else {
     model <- data
   }
+
+  faminfo <- insight::model_info(model)
+  r <- tryCatch(
+    {
+      if (inherits(model, "merMod")) {
+        stats::residuals(model, scaled = TRUE)
+      } else if (inherits(model, c("glmmTMB", "MixMod"))) {
+        sigma <- if (faminfo$is_mixed) {
+          sqrt(insight::get_variance_residual(model))
+        } else {
+          .sigma_glmmTMB_nonmixed(model, faminfo)
+        }
+        stats::residuals(model) / sigma
+      } else if (inherits(model, "glm")) {
+        stats::rstandard(model, type = "pearson")
+      } else {
+        stats::rstandard(model)
+      }
+    },
+    error = function(e) {
+      NULL
+    }
+  )
+
+  if (is.null(r)) {
+    warning(sprintf("Homogeneity of variance could not be computed. Cannot extract residual variance from objects of class '%s'.\n", class(model)[1]), call. = FALSE)
+    return(NULL)
+  }
+
   dat <- data.frame(
     x = stats::fitted(model),
-    y = stats::residuals(model)
+    y = sqrt(abs(r))
   )
-  .plot_diag_linearity(dat, size_point = 2, size_line = .8)
+  .plot_diag_homogeneity(dat, size_point = 2, size_line = .8, ...)
+}
+
+
+.sigma_glmmTMB_nonmixed <- function(model, faminfo) {
+  if (!is.na(match(faminfo$family, c("binomial", "poisson", "truncated_poisson")))) {
+    return(1)
+  }
+  betad <- model$fit$par["betad"]
+  switch(faminfo$family,
+         gaussian = exp(0.5 * betad),
+         Gamma = exp(-0.5 * betad),
+         exp(betad)
+  )
 }
