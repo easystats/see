e401401e217fc642c5ac9e64b75e8e04183a84e7
Daniel <mail@danielluedecke.de>
2021-04-25 13:07:59 +0200

    test conditional

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,20 +1,21 @@
-library(testthat)
-library(see)
+if (require("testthat")) {
+  library(see)
 
-osx <- tryCatch(
-  {
-    si <- Sys.info()
-    if (!is.null(si["sysname"])) {
-      si["sysname"] == "Darwin" || grepl("^darwin", R.version$os)
-    } else {
+  osx <- tryCatch(
+    {
+      si <- Sys.info()
+      if (!is.null(si["sysname"])) {
+        si["sysname"] == "Darwin" || grepl("^darwin", R.version$os)
+      } else {
+        FALSE
+      }
+    },
+    error = function(e) {
       FALSE
     }
-  },
-  error = function(e) {
-    FALSE
-  }
-)
+  )
 
-if (!osx) {
-  test_check("see")
+  if (!osx) {
+    test_check("see")
+  }
 }
