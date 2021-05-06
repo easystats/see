4c13542531b86ca58d7bc55041ac3cb45ddf730a
Indrajeet Patil <inderonline1988@gmail.com>
2021-05-06 18:54:06 +0200

    skip on older R versions

--- tests/testthat/test-plot.check_normality.R
+++ tests/testthat/test-plot.check_normality.R
@@ -1,46 +1,45 @@
 if (getRversion() < "4.1" && require("vdiffr") && require("ggplot2") &&
-    require("performance") && require("lme4") && require("see") && require("qqplotr")) {
+  require("performance") && require("lme4") && require("see") && require("qqplotr")) {
+  test_that("`plot.see_check_normality()` works", {
+    skip_if(getRversion() < "3.6")
+    skip_on_cran()
 
-test_that("`plot.see_check_normality()` works", {
- skip_on_cran()
+    set.seed(123)
+    m_lm <<- lm(mpg ~ wt + cyl + gear + disp, data = mtcars)
+    result1 <- check_normality(m_lm)
+    result2 <- check_normality(m_lm, type = "qq")
+    result3 <- check_normality(m_lm, type = "pp")
 
-  set.seed(123)
-  m_lm <<- lm(mpg ~ wt + cyl + gear + disp, data = mtcars)
-  result1 <- check_normality(m_lm)
-  result2 <- check_normality(m_lm, type = "qq")
-  result3 <- check_normality(m_lm, type = "pp")
+    set.seed(123)
+    m_lmer <<- lmer(Reaction ~ Days + (Days | Subject), sleepstudy)
+    result4 <- check_normality(m_lmer, "random")
+    result5 <- check_normality(m_lmer, "fixed")
 
-  set.seed(123)
-  m_lmer <<- lmer(Reaction ~ Days + (Days | Subject), sleepstudy)
-  result4 <- check_normality(m_lmer, "random")
-  result5 <- check_normality(m_lmer, "fixed")
+    set.seed(123)
+    vdiffr::expect_doppelganger(
+      title = "check_normality works - lm - density",
+      fig = plot(result1)
+    )
 
-  set.seed(123)
-  vdiffr::expect_doppelganger(
-    title = "check_normality works - lm - density",
-    fig = plot(result1)
-  )
+    vdiffr::expect_doppelganger(
+      title = "check_normality works - lm - qq",
+      fig = plot(result2)
+    )
 
-  vdiffr::expect_doppelganger(
-    title = "check_normality works - lm - qq",
-    fig = plot(result2)
-  )
+    vdiffr::expect_doppelganger(
+      title = "check_normality works - lm - pp",
+      fig = plot(result3)
+    )
 
-  vdiffr::expect_doppelganger(
-    title = "check_normality works - lm - pp",
-    fig = plot(result3)
-  )
+    set.seed(123)
+    vdiffr::expect_doppelganger(
+      title = "check_normality works - lmer - random",
+      fig = plot(result4)
+    )
 
-  set.seed(123)
-  vdiffr::expect_doppelganger(
-    title = "check_normality works - lmer - random",
-    fig = plot(result4)
-  )
-
-  vdiffr::expect_doppelganger(
-    title = "check_normality works - lmer - fixed",
-    fig = plot(result5)
-  )
-
-})
+    vdiffr::expect_doppelganger(
+      title = "check_normality works - lmer - fixed",
+      fig = plot(result5)
+    )
+  })
 }
