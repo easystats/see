af244d4ed723106fb15ce16b13ecabd89f17c431
Indrajeet Patil <inderonline1988@gmail.com>
2021-05-06 18:46:29 +0200

    visual regression tests for check normality

--- tests/testthat/test-plot.check_normality.R
+++ tests/testthat/test-plot.check_normality.R
@@ -1,6 +1,46 @@
+if (getRversion() < "4.1" && require("vdiffr") && require("ggplot2") &&
+    require("performance") && require("lme4") && require("see") && require("qqplotr")) {
+
 test_that("`plot.see_check_normality()` works", {
-  library(performance)
-  m <<- lm(mpg ~ wt + cyl + gear + disp, data = mtcars)
-  result <- check_normality(m)
-  expect_s3_class(plot(result), "gg")
+ skip_on_cran()
+
+  set.seed(123)
+  m_lm <<- lm(mpg ~ wt + cyl + gear + disp, data = mtcars)
+  result1 <- check_normality(m_lm)
+  result2 <- check_normality(m_lm, type = "qq")
+  result3 <- check_normality(m_lm, type = "pp")
+
+  set.seed(123)
+  m_lmer <<- lmer(Reaction ~ Days + (Days | Subject), sleepstudy)
+  result4 <- check_normality(m_lmer, "random")
+  result5 <- check_normality(m_lmer, "fixed")
+
+  set.seed(123)
+  vdiffr::expect_doppelganger(
+    title = "check_normality works - lm - density",
+    fig = plot(result1)
+  )
+
+  vdiffr::expect_doppelganger(
+    title = "check_normality works - lm - qq",
+    fig = plot(result2)
+  )
+
+  vdiffr::expect_doppelganger(
+    title = "check_normality works - lm - pp",
+    fig = plot(result3)
+  )
+
+  set.seed(123)
+  vdiffr::expect_doppelganger(
+    title = "check_normality works - lmer - random",
+    fig = plot(result4)
+  )
+
+  vdiffr::expect_doppelganger(
+    title = "check_normality works - lmer - fixed",
+    fig = plot(result5)
+  )
+
 })
+}
