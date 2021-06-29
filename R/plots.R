c77d7c4df1031a547929c2678efb711a3d6e8dc0
Indrajeet Patil <inderonline1988@gmail.com>
2021-06-29 22:40:10 +0200

    style

--- R/plots.R
+++ R/plots.R
@@ -1,16 +1,35 @@
 #' Multiple plots side by side
 #'
-#' A wrapper around \emph{patchwork} to plot multiple figures side by side on the same page. See \href{https://patchwork.data-imaginist.com/articles/patchwork.html}{the \emph{patchwork} documentation} for more advanced control of plot layouts.
+#' A wrapper around \emph{patchwork} to plot multiple figures side by side on
+#' the same page. See
+#' \href{https://patchwork.data-imaginist.com/articles/patchwork.html}{the
+#' \emph{patchwork} documentation} for more advanced control of plot layouts.
 #'
 #' @param ... Multiple \code{ggplot}s or a list containing \code{ggplot} objects
 #' @param n_rows Number of rows to align plots.
 #' @param n_columns Number of columns to align plots.
-#' @param guides 	A string specifying how guides should be treated in the layout. \code{'collect'} will collect shared guides across plots, removing duplicates. \code{'keep'} will keep guides alongside their plot. \code{'auto'} will inherit from a higher patchwork level (if any). See \code{\link[patchwork:plot_layout]{patchwork::plot_layout()}} for details.
-#' @param tags Add tags to your subfigures. Can be \code{NULL} to omit (default) or a character vector containing tags for each plot. Automatic tags can also be generated with \code{'1'} for Arabic numerals, \code{'A'} for uppercase Latin letters, \code{'a'} for lowercase Latin letters, \code{'I'} for uppercase Roman numerals, and \code{'i'} for lowercase Roman numerals. For backwards compatibility, can also be \code{FALSE} (equivalent to \code{NULL}), \code{NA} (equivalent to \code{NULL}), or \code{TRUE} (equivalent to \code{'A'}).
-#' @param tag_prefix,tag_suffix Text strings that should appear before or after the tag.
-#' @param tag_sep Text string giving the separator to use between different tag levels.
-#' @param title,subtitle,caption Text strings to use for the various plot annotations to add to the composed patchwork.
-#' @param theme A ggplot theme specification to use for the plot. Only elements related to titles, caption, and tags, as well as plot margin and background, are used.
+#' @param guides A string specifying how guides should be treated in the
+#'   layout. \code{'collect'} will collect shared guides across plots, removing
+#'   duplicates. \code{'keep'} will keep guides alongside their plot.
+#'   \code{'auto'} will inherit from a higher patchwork level (if any). See
+#'   \code{\link[patchwork:plot_layout]{patchwork::plot_layout()}} for details.
+#' @param tags Add tags to your subfigures. Can be \code{NULL} to omit (default)
+#'   or a character vector containing tags for each plot. Automatic tags can
+#'   also be generated with \code{'1'} for Arabic numerals, \code{'A'} for
+#'   uppercase Latin letters, \code{'a'} for lowercase Latin letters, \code{'I'}
+#'   for uppercase Roman numerals, and \code{'i'} for lowercase Roman numerals.
+#'   For backwards compatibility, can also be \code{FALSE} (equivalent to
+#'   \code{NULL}), \code{NA} (equivalent to \code{NULL}), or \code{TRUE}
+#'   (equivalent to \code{'A'}).
+#' @param tag_prefix,tag_suffix Text strings that should appear before or after
+#'   the tag.
+#' @param tag_sep Text string giving the separator to use between different tag
+#'   levels.
+#' @param title,subtitle,caption Text strings to use for the various plot
+#'   annotations to add to the composed patchwork.
+#' @param theme A ggplot theme specification to use for the plot. Only elements
+#'   related to titles, caption, and tags, as well as plot margin and
+#'   background, are used.
 #' @examples
 #' library(ggplot2)
 #' library(see)
@@ -24,27 +43,35 @@
 #'   scale_x_discrete("cyl")
 #'
 #' plots(p1, p2)
-#' plots(p1, p2, n_columns = 2, tags = 'A')
+#' plots(p1, p2, n_columns = 2, tags = "A")
 #' plots(
 #'   p1, p2, p3,
 #'   n_columns = 1, tags = c("Fig. 1", "Fig. 2", "Fig. 3"),
 #'   title = "The surprising truth about mtcars"
 #' )
 #' @export
-plots <- function(..., n_rows = NULL, n_columns = NULL, guides = NULL,
-                  tags = FALSE, tag_prefix = NULL, tag_suffix = NULL, tag_sep = NULL,
-                  title = NULL, subtitle = NULL, caption = NULL,
+plots <- function(...,
+                  n_rows = NULL,
+                  n_columns = NULL,
+                  guides = NULL,
+                  tags = FALSE,
+                  tag_prefix = NULL,
+                  tag_suffix = NULL,
+                  tag_sep = NULL,
+                  title = NULL,
+                  subtitle = NULL,
+                  caption = NULL,
                   theme = NULL) {
   insight::check_if_installed("patchwork")
 
-    # Add tags
+  # Add tags
   if (!is.null(tags)) {
     if (length(tags) == 1) {
       if (isTRUE(tags)) {
-        tags <- 'A'
+        tags <- "A"
       } else if (isFALSE(tags) || is.na(tags)) {
         tags <- NULL
-      } else if (! tags %in% c('1', 'A', 'a', 'I', 'i')) {
+      } else if (!tags %in% c("1", "A", "a", "I", "i")) {
         tags <- list(tags)
       }
     } else {
@@ -61,4 +88,3 @@ plots <- function(..., n_rows = NULL, n_columns = NULL, guides = NULL,
     )
   return(pw)
 }
-
