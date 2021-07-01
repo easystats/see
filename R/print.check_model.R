b313a9c0487348bf7d45f7f39f01fe3e2f389341
Brenton M. Wiernik <bwiernik@users.noreply.github.com>
2021-07-01 11:00:03 -0400

    Fix regression from ffc433c94d54a4f2d7a3193c21a5da2c385bb73b
    Closes https://github.com/easystats/see/issues/163

--- R/print.check_model.R
+++ R/print.check_model.R
@@ -3,6 +3,6 @@ print.see_check_model <- function(x,
                                   style = theme_lucid,
                                   colors = c("#3aaf85", "#1b6ca8", "#cd201f"),
                                   ...) {
-  plot(x, style = style, colors = colors, ...)
+  suppressWarnings(suppressMessages(print(plot(x, style = style, colors = colors, ...))))
   invisible(x)
 }
