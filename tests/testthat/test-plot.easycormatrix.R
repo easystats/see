5d30785ff2a37b2cfc866d6c73c92bcff2733932
Daniel <mail@danielluedecke.de>
2022-01-13 09:32:22 +0100

    fix test

--- tests/testthat/test-plot.easycormatrix.R
+++ tests/testthat/test-plot.easycormatrix.R
@@ -1,4 +1,4 @@
-if (require("correlation") && require("see")) {
+if (require("correlation") && require("testthat") && require("see") && utils::packageVersion("correlation") > "0.7.1") {
   test_that("`plot.see_easycormatrix()` works", {
     result <- correlation(mtcars[, -c(8:9)])
     s <- summary(result)
