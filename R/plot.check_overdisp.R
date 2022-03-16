9efbc857581d3584a4166a273920b00018dc0bf0
Daniel <mail@danielluedecke.de>
2022-03-16 23:01:50 +0100

    no docs

--- R/plot.check_overdisp.R
+++ R/plot.check_overdisp.R
@@ -1,22 +1,3 @@
-#' Plot method to check model for overdispersion
-#'
-#' The `plot()` method for the `performance::check_overdispersion()`
-#' function.
-#'
-#' @param size_line Numeric value specifying size of line geoms.
-#' @inheritParams data_plot
-#' @inheritParams plot.see_check_normality
-#'
-#' @return A ggplot2-object.
-#'
-#' @examples
-#' library(performance)
-#' if (require("glmmTMB")) {
-#'   data(Salamanders)
-#'   m <- glm(count ~ spp + mined, family = poisson, data = Salamanders)
-#'   result <- check_overdispersion(m)
-#'   plot(result)
-#' }
 #' @export
 plot.see_check_overdisp <- function(x,
                                     size_line = .8,
