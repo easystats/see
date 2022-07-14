25a8c925ab61457aab15f99e819183b0bc99f930
Daniel <mail@danielluedecke.de>
2022-07-14 19:11:51 +0200

    format msg

--- R/plots.R
+++ R/plots.R
@@ -94,13 +94,22 @@ plots <- function(...,
   return(pw)
 }
 
+
 .safe_print_plots <- function(pw) {
   pw_drawn <- tryCatch(print(pw), error = function(e) e)
   if (inherits(pw_drawn, "simpleError")) {
     if (Sys.getenv("RSTUDIO") == "1") {
-      stop("The RStudio 'Plots' window is too small to show this set of plots.\n Please make the window larger.", call. = FALSE)
+      stop(insight::format_message(
+        "The RStudio 'Plots' window is too small to show this set of plots.",
+        "Please make the window larger."),
+        call. = FALSE
+      )
     } else {
-      stop("The viewport is too small to show this set of plots.\n Please make it larger.", call. = FALSE)
+      stop(insight::format_message(
+        "The viewport is too small to show this set of plots.",
+        "Please make it larger."),
+        call. = FALSE
+      )
     }
   }
 }
