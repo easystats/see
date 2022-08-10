37d5da8af3d8655bfa280cc4e1b1b0a4b607b773
Daniel <mail@danielluedecke.de>
2022-08-10 10:06:30 +0200

    run test conditionally

--- tests/testthat/test-plot.cluster_analysis.R
+++ tests/testthat/test-plot.cluster_analysis.R
@@ -1,4 +1,4 @@
-if (require("parameters") && require("see")) {
+if (require("parameters") && require("see") && utils::packageVersion("parameters") > "0.18.1") {
   test_that("`plot.see_cluster_analysis()` works", {
     groups <- cluster_analysis(iris[, 1:4], 3)
     expect_s3_class(plot(groups), "gg")
