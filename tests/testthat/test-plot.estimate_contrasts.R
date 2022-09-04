2ab46c8a11332b6c766dc0a6d56af823c82969e4
Indrajeet Patil <patilindrajeet.science@gmail.com>
2022-09-04 22:26:10 +0200

    Fix failing test

--- tests/testthat/test-plot.estimate_contrasts.R
+++ tests/testthat/test-plot.estimate_contrasts.R
@@ -1,8 +1,10 @@
 test_that("`plot.see_estimate_contrasts()` works", {
-  if (require("modelbased") && require("rstanarm")) {
+  skip_if_not_installed("modelbased")
+  skip_if_not_installed("rstanarm")
+  skip_if_not_installed("emmeans")
+
     model <- stan_glm(Sepal.Width ~ Species, data = iris, refresh = 0)
     contrasts <- estimate_contrasts(model)
     means <- estimate_means(model)
     expect_s3_class(plot(contrasts, means), "gg")
-  }
 })
