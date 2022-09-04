7b6b4d6dc4d32bebe4b444cebe3d58c192f9f079
Indrajeet Patil <patilindrajeet.science@gmail.com>
2022-09-04 22:39:35 +0200

    Update test-plot.estimate_contrasts.R

--- tests/testthat/test-plot.estimate_contrasts.R
+++ tests/testthat/test-plot.estimate_contrasts.R
@@ -3,8 +3,8 @@ test_that("`plot.see_estimate_contrasts()` works", {
   skip_if_not_installed("rstanarm")
   skip_if_not_installed("emmeans")
 
-    model <- stan_glm(Sepal.Width ~ Species, data = iris, refresh = 0)
-    contrasts <- estimate_contrasts(model)
-    means <- estimate_means(model)
+    model <- rstanarm::stan_glm(Sepal.Width ~ Species, data = iris, refresh = 0)
+    contrasts <- modelbased::estimate_contrasts(model)
+    means <- modelbased::estimate_means(model)
     expect_s3_class(plot(contrasts, means), "gg")
 })
