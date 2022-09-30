48f18bbb1377b24ea65e5e71907ab49a31006523
Indrajeet Patil <patilindrajeet.science@gmail.com>
2022-09-30 12:02:35 +0200

    Add package meta-data

--- R/see-package.R
+++ R/see-package.R
@@ -1,3 +1,21 @@
+#' \code{see}
+#'
+#' @title see: Framework for Easy Statistical Modeling, Visualization, and Reporting
+#'
+#' @description
+#'
+#' Provides plotting utilities supporting packages in the 'easystats' ecosystem
+#' (<https://github.com/easystats/easystats>) and some extra themes, geoms, and
+#' scales for 'ggplot2'. Color scales are based on
+#' <https://materialui.co/colors>.
+#'
+#' References: Lüdecke et al. (2021) <doi:10.21105/joss.03393>.
+#'
+#' @docType package
+#' @aliases see see-package
+#' @name see-package
+"_PACKAGE"
+
 # Suppress R CMD check note
 # Namespace in Imports field not imported from: PKG
 #   All declared Imports should be used.
