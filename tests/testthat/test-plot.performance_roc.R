bf781586b529c0f9d82f49ce1e87a1aec54e05bd
Indrajeet Patil <patilindrajeet.science@gmail.com>
2022-11-26 07:44:32 +0100

    CRAN RC see 0.7.4 (#252)
    * CRAN RC see 0.7.4
    
    * fewer conditionals in tests
    
    https://github.com/easystats/easystats/issues/310
    
    * Update DESCRIPTION

--- tests/testthat/test-plot.performance_roc.R
+++ tests/testthat/test-plot.performance_roc.R
@@ -1,17 +1,15 @@
 test_that("`plot.see_performance_roc()` works", {
-  if (packageVersion("performance") > "0.7.0") {
-    library(performance)
-    set.seed(123)
-    iris$y <- rbinom(nrow(iris), size = 1, 0.3)
-    folds <- sample(nrow(iris), size = nrow(iris) / 8, replace = FALSE)
-    test_data <- iris[folds, ]
-    train_data <- iris[-folds, ]
-    model <- glm(y ~ Sepal.Length + Sepal.Width,
-      data = train_data,
-      family = "binomial"
-    )
-    result <- performance_roc(model, new_data = test_data)
+  library(performance)
+  set.seed(123)
+  iris$y <- rbinom(nrow(iris), size = 1, 0.3)
+  folds <- sample(nrow(iris), size = nrow(iris) / 8, replace = FALSE)
+  test_data <- iris[folds, ]
+  train_data <- iris[-folds, ]
+  model <- glm(y ~ Sepal.Length + Sepal.Width,
+    data = train_data,
+    family = "binomial"
+  )
+  result <- performance_roc(model, new_data = test_data)
 
-    expect_s3_class(plot(result), "gg")
-  }
+  expect_s3_class(plot(result), "gg")
 })
