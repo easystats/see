5af0b94e0bb3b9900b016d540191b0f1e9234e1f
Indrajeet Patil <patilindrajeet.science@gmail.com>
2022-11-27 17:44:35 +0100

    Update helpers.R

--- tests/testthat/helpers.R
+++ tests/testthat/helpers.R
@@ -4,3 +4,14 @@ requiet <- function(package) {
     require(package, warn.conflicts = FALSE, character.only = TRUE)
   )
 }
+
+# load hard deps to use them without namespacing
+library(bayestestR)
+library(correlation)
+library(datawizard)
+library(effectsize)
+suppressPackageStartupMessages(library(ggplot2))
+library(insight)
+library(modelbased)
+library(parameters)
+library(performance)
