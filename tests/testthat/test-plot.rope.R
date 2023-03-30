64d2731ec84bd73910d8296b8fc16020c7924f86
Indrajeet Patil <patilindrajeet.science@gmail.com>
2023-03-30 10:36:01 +0200

    suppress messages

--- tests/testthat/test-plot.rope.R
+++ tests/testthat/test-plot.rope.R
@@ -2,7 +2,7 @@ test_that("`plot.see_rope()` works", {
   skip_if_not_installed("rstanarm")
 
   set.seed(123)
-  result <- bayestestR::rope(m_rstan)
+  result <- bayestestR::rope(m_rstan, verbose = FALSE)
 
   expect_s3_class(plot(result), "gg")
 })
