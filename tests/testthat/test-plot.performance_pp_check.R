51c437d024be9378a31d0cb34ffb2531f0ebd92a
Daniel <mail@danielluedecke.de>
2023-04-03 18:37:21 +0200

    Figure legend in check predictions (#282)

--- tests/testthat/test-plot.performance_pp_check.R
+++ tests/testthat/test-plot.performance_pp_check.R
@@ -1,5 +1,10 @@
 test_that("`print.see_performance_pp_check()` works", {
+  set.seed(123)
   model <- lm(Sepal.Length ~ Species * Petal.Width + Petal.Length, data = iris)
   result <- performance::check_posterior_predictions(model)
-  expect_s3_class(plot(result), "gg")
+
+  vdiffr::expect_doppelganger(
+    title = "pp check - lm",
+    fig = plot(result)
+  )
 })
