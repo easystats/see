f6fdf1d6c37cebf2b101f43c597f9ba3efbe1bcf
Etienne Bacher <52219252+etiennebacher@users.noreply.github.com>
2023-05-30 13:57:28 +0200

    add args for na label and to remove na, add modern theme

--- R/plot.dw_data_tabulate.R
+++ R/plot.dw_data_tabulate.R
@@ -1,26 +1,43 @@
 #' @export
 
-plot.dw_data_tabulates <- function(x, value_lab = TRUE) {
+plot.dw_data_tabulates <- function(x, value_lab = TRUE, remove_na = FALSE,
+                                   na_label = "(Missing)") {
   lapply(x, function(dat) {
-    plot.dw_data_tabulate(dat, value_lab = value_lab)
+    plot.dw_data_tabulate(dat, value_lab = value_lab, remove_na = remove_na,
+                          na_label = na_label)
   })
 }
 
 #' @export
 
-plot.dw_data_tabulate <- function(x, value_lab = TRUE) {
+plot.dw_data_tabulate <- function(x, value_lab = TRUE, remove_na = FALSE,
+                                  na_label = "(Missing)") {
 
   dat <- as.data.frame(x)
-  dat$label <- paste0(dat$N, "\n(", round(dat$`Raw %`, 2), "%)")
+
+  if (isTRUE(remove_na)) {
+    dat <- dat[!is.na(dat$Value), ]
+    dat$output <- dat$`Valid %`
+  } else {
+    dat$output <- dat$`Raw %`
+
+    # deal with factors
+    dat$Value <- as.character(dat$Value)
+    dat$Value[is.na(dat$Value)] <- na_label
+    dat$Value <- factor(dat$Value, levels = c(setdiff(dat$Value, na_label), na_label))
+  }
+
+  dat$label <- paste0(dat$N, "\n(", round(dat$output, 2), "%)")
 
   out <- ggplot(dat, aes(x = Value, y = N)) +
     geom_col() +
-    labs(title = unique(dat$Variable))
+    labs(title = unique(dat$Variable)) +
+    theme_modern()
 
   if (isTRUE(value_lab)) {
      out <- out +
        geom_text(aes(label = label), vjust = -0.5) +
-       ylim(c(0, max(dat$N) * 1.1))
+       ylim(c(0, max(dat$N) * 1.2))
   }
 
   out
