8f64c618f7851e6aac8fbf3648c83f167c3f31d9
Etienne Bacher <52219252+etiennebacher@users.noreply.github.com>
2023-06-02 14:05:16 +0200

    try adding error bars

--- R/plot.dw_data_tabulate.R
+++ R/plot.dw_data_tabulate.R
@@ -1,33 +1,47 @@
 #' @export
 
 plot.dw_data_tabulates <- function(x, value_lab = TRUE, remove_na = FALSE,
-                                   na_label = "(Missing)") {
+                                   na_label = "(Missing)", error_bar = TRUE) {
   lapply(x, function(dat) {
     plot.dw_data_tabulate(dat, value_lab = value_lab, remove_na = remove_na,
-                          na_label = na_label)
+                          na_label = na_label, error_bar = error_bar)
   })
 }
 
 #' @export
 
 plot.dw_data_tabulate <- function(x, value_lab = TRUE, remove_na = FALSE,
-                                  na_label = "(Missing)") {
+                                  na_label = "(Missing)", error_bar = TRUE) {
 
   dat <- as.data.frame(x)
 
   if (isTRUE(remove_na)) {
     dat <- dat[!is.na(dat$Value), ]
-    dat$output <- dat$`Valid %`
+    dat$output <- dat[[which(startsWith(names(dat), "Valid"))]]
   } else {
-    dat$output <- dat$`Raw %`
+    dat$output <- dat[[which(startsWith(names(dat), "Raw"))]]
 
-    # deal with factors
+    # deal with missing values
     dat$Value <- as.character(dat$Value)
     dat$Value[is.na(dat$Value)] <- na_label
-    dat$Value <- factor(dat$Value, levels = c(setdiff(dat$Value, na_label), na_label))
+    dat$Value <- factor(
+      dat$Value, levels = c(setdiff(dat$Value, na_label), na_label)
+    )
   }
 
-  dat$label <- paste0(dat$N, "\n(", round(dat$output, 2), "%)")
+  if (isTRUE(error_bar)) {
+    total_n <- sum(dat$N)
+    rel_frq <- dat$output/100
+    ci <- 1.96 * suppressWarnings(sqrt(rel_frq * (1 - rel_frq) / total_n))
+    dat$upper.ci <- total_n * (rel_frq + ci)
+    dat$lower.ci <- total_n * (rel_frq - ci)
+  }
+
+  if (isTRUE(error_bar)) {
+    dat$label <- paste0(dat$N, " (", round(dat$output, 2), "%)")
+  } else {
+    dat$label <- paste0(dat$N, "\n(", round(dat$output, 2), "%)")
+  }
 
   out <- ggplot(dat, aes(x = Value, y = N)) +
     geom_col() +
@@ -35,9 +49,21 @@ plot.dw_data_tabulate <- function(x, value_lab = TRUE, remove_na = FALSE,
     theme_modern()
 
   if (isTRUE(value_lab)) {
-     out <- out +
-       geom_text(aes(label = label), vjust = -0.5) +
-       ylim(c(0, max(dat$N) * 1.2))
+    if (isTRUE(error_bar)) {
+      out <- out +
+        geom_text(aes(label = label), vjust = -1, hjust = 1.2) +
+        ylim(c(0, max(dat$N) * 1.5))
+    } else {
+      out <- out +
+        geom_text(aes(label = label), vjust = -0.5) +
+        ylim(c(0, max(dat$N) * 1.2))
+    }
+  }
+
+  # add confidence intervals for frequencies
+  if (isTRUE(error_bar)) {
+    out <- out +
+      geom_errorbar(aes(ymin = lower.ci, ymax = upper.ci), width = 0.5, color = "darkblue")
   }
 
   out
