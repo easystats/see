e0ce62c644ce36ce75a2039777e4d6b5c4527394
Indrajeet Patil <patilindrajeet.science@gmail.com>
2023-09-13 19:09:58 +0200

    skip tests on devel

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,6 +1,6 @@
-# graphics engine changed in R 4.1, and so snapshots generated on
-# previous R version won't be compatible
-if (require("testthat") && require("vdiffr") && getRversion() > "4.1.0") {
+# graphics engine changed in these versions, and so snapshots generated on
+# previous R version won't work
+if (require("testthat") && require("vdiffr") && getRversion() > "4.1.0" && getRversion() < "4.4.0") {
   library(testthat)
   library(see)
 
