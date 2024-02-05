10d812de7cd756a23d775f494d23108cb80c7a8e
Daniel <mail@danielluedecke.de>
2024-02-05 21:43:53 +0100

    revert change (https://github.com/easystats/performance/issues/654)

--- R/plot.check_normality.R
+++ R/plot.check_normality.R
@@ -174,7 +174,7 @@ plot.see_check_normality <- function(x,
                           model_class = NULL) {
   qhalfnorm <- function(p) stats::qnorm((p + 1) / 2)
   # qq-halfnorm for GLM
-  if (!identical(model_class, "glmmTMB") && (isTRUE(model_info$is_binomial) || isTRUE(model_info$is_count))) {
+  if (isTRUE(model_info$is_binomial) || isTRUE(model_info$is_count)) {
     gg_init <- ggplot2::ggplot(x, ggplot2::aes(x = .data$x, y = .data$y))
     qq_stuff <- list(
       ggplot2::geom_point(
