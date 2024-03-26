42e0e708e379ba94cb7467816a28b2f0b13350bc
Daniel <mail@danielluedecke.de>
2024-03-26 11:48:24 +0100

    add check

--- tests/testthat/test-check_model.R
+++ tests/testthat/test-check_model.R
@@ -111,3 +111,18 @@ test_that("`check_model()` works if convergence issues", {
   x <- performance::check_model(m)
   expect_s3_class(x, "check_model")
 })
+
+
+
+test_that("`check_model()` works if convergence issues", {
+  skip_on_cran()
+  skip_if_not_installed("performance")
+  data(mtcars)
+  m <- lm(mpg ~ wt + cyl + gear + disp, data = mtcars)
+  set.seed(123)
+  x <- performance::check_model(m)
+  vdiffr::expect_doppelganger(
+    title = "check_model works for lm",
+    fig = plot(x)
+  )
+})
