3b0ff6f7e5c2ce1eed2b24bcf104c2a5979682ca
Indrajeet Patil <patilindrajeet.science@gmail.com>
2024-04-27 19:02:32 +0200

    effectsize vdiffr test

--- tests/testthat/test-plot.effectsize_table.R
+++ tests/testthat/test-plot.effectsize_table.R
@@ -1,13 +1,9 @@
 test_that("`plot.see_effectsize_table()` works", {
-  library(effectsize)
   m <- aov(mpg ~ factor(am) * factor(cyl), data = mtcars)
-  result <- eta_squared(m)
-  expect_s3_class(plot(result), "gg")
-})
-
-test_that("`plot.see_equivalence_test()` works", {
-  library(effectsize)
-  m <- aov(mpg ~ factor(am) * factor(cyl), data = mtcars)
-  result <- eta_squared(m)
-  expect_s3_class(plot(result), "gg")
+  result <- effectsize::eta_squared(m)
+  set.seed(123)
+  vdiffr::expect_doppelganger(
+    title = "aov - eta_squared - dot plot",
+    fig = plot(result)
+  )
 })
