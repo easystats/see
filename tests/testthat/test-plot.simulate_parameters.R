e0784a5cee378adf40581e687d14e7f0fc640291
Daniel <mail@danielluedecke.de>
2024-09-29 13:43:56 +0200

    add test

---
+++ tests/testthat/test-plot.simulate_parameters.R
@@ -0,0 +1,46 @@
+skip_if(TRUE)
+
+## FIXME: currently does not retrieve the data
+
+skip_on_cran()
+skip_if_offline()
+skip_if_not_installed("withr")
+skip_if_not_installed("mclogit")
+skip_if_not_installed("parameters")
+skip_if_not_installed("vdiffr")
+
+withr::with_environment(
+  new.env(),
+  test_that("`plot()` for simulate_parameters", {
+    pict <- base::readRDS(url("https://slcladal.github.io/data/pict.rda", "rb"))
+    suppressWarnings({
+      m1.mn <- mclogit::mblogit(
+        formula = Response ~ Gender + Group,
+        random = ~ 1 | Item,
+        data = pict
+      )
+    })
+    set.seed(1234)
+    ms <- parameters::simulate_parameters(m1.mn)
+    set.seed(1234)
+    vdiffr::expect_doppelganger(
+      title = "plot.simulate_parameters works",
+      fig = plot(ms)
+    )
+    set.seed(1234)
+    vdiffr::expect_doppelganger(
+      title = "plot.simulate_parameters works-2",
+      fig = plot(ms, stack = FALSE)
+    )
+    set.seed(1234)
+    vdiffr::expect_doppelganger(
+      title = "plot.simulate_parameters works-3",
+      fig = plot(ms, stack = FALSE, show_intercept = TRUE, normalize_height = TRUE)
+    )
+    set.seed(1234)
+    vdiffr::expect_doppelganger(
+      title = "plot.simulate_parameters works-4",
+      fig = plot(ms, stack = TRUE, show_intercept = TRUE, normalize_height = TRUE)
+    )
+  })
+)
