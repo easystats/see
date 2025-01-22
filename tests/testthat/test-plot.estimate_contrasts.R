196e5592ecbe725946747d44476a3dfbab875aee
Indrajeet Patil <patilindrajeet.science@gmail.com>
2025-01-22 08:45:46 +0100

    add missing dep

--- tests/testthat/test-plot.estimate_contrasts.R
+++ tests/testthat/test-plot.estimate_contrasts.R
@@ -1,6 +1,7 @@
 test_that("`plot.see_estimate_contrasts()` works", {
   skip_if_not_installed("rstanarm")
   skip_if_not_installed("emmeans")
+  skip_if_not_installed("marginaleffects")
 
   model <- stan_glm(Sepal.Width ~ Species, data = iris, refresh = 0)
   contrasts <- modelbased::estimate_contrasts(model, contrast = "Species")
