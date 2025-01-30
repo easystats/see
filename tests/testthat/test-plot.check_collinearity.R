2123d0a109c1132d95d90b34355b584c13e7184f
Daniel <mail@danielluedecke.de>
2025-01-30 22:50:32 +0100

    update tests

--- tests/testthat/test-plot.check_collinearity.R
+++ tests/testthat/test-plot.check_collinearity.R
@@ -1,7 +1,7 @@
 test_that("`plot.see_check_collinearity()` works", {
   m <- lm(mpg ~ wt + cyl + gear + disp, data = mtcars)
   result <- performance::check_collinearity(m)
-  expect_s3_class(plot(result), "gg")
+  expect_s3_class(plot(result), c("gg", "ggplot"))
 
   skip_if_not_installed("vdiffr")
   vdiffr::expect_doppelganger(
