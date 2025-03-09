eb2c8898c6e139c7e06297a58a8dad225c850ec2
Daniel <mail@danielluedecke.de>
2025-03-09 17:27:27 +0100

    add test

--- tests/testthat/test-plot.estimate_density.R
+++ tests/testthat/test-plot.estimate_density.R
@@ -7,3 +7,18 @@ test_that("`plot.see_estimate_density()` works", {
   result <- bayestestR::estimate_density(m_rstan)
   expect_s3_class(plot(result), "gg")
 })
+
+
+test_that("`plot.see_estimate_density()`, adding prior layers works", {
+  skip_if_not_installed("rstanarm")
+  skip_if_not_installed("ggridges")
+  skip_if_not_installed("vdiffr")
+
+  set.seed(123)
+  result <- bayestestR::estimate_density(m_rstan)
+  set.seed(123)
+  vdiffr::expect_doppelganger(
+    title = "plot.estimate_density with piors",
+    fig = plot(result, stack = FALSE, priors = TRUE)
+  )
+})
