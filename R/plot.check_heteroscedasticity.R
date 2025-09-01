0a30ef95b1c0e8b776e093382cafab25efea0603
Daniel <mail@danielluedecke.de>
2025-09-01 20:00:08 +0200

    Update R/plot.check_heteroscedasticity.R
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- R/plot.check_heteroscedasticity.R
+++ R/plot.check_heteroscedasticity.R
@@ -96,7 +96,8 @@ plot.see_check_heteroscedasticity <- function(
   }
   betad <- model$fit$par["betad"]
 
-  switch(faminfo$family,
+  switch(
+    faminfo$family,
     gaussian = exp(0.5 * betad),
     Gamma = exp(-0.5 * betad),
     exp(betad)
