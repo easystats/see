a416ac380e9d18941d500ca3e5ad410a35a198ea
Daniel <mail@danielluedecke.de>
2026-05-22 12:12:31 +0200

    test

--- tests/testthat/test-vdiffr_factor_analysis.R
+++ tests/testthat/test-vdiffr_factor_analysis.R
@@ -3,6 +3,7 @@ skip_if_not_installed("vdiffr")
 skip_if_not_installed("parameters")
 skip_if_not_installed("ggraph")
 skip_if_not_installed("tidygraph")
+skip_if_not_installed("psych")
 
 # Helper function for reproducible vdiffr tests
 expect_doppelganger_with_seed <- function(title, fig, seed = 123) {
@@ -30,8 +31,8 @@ test_that("plot.see_check_model() renders correctly", {
   )
 
   expect_doppelganger_with_seed(
-    title = "plot FA, graph",
-    fig = plot(f, type = "graph-1")
+    title = "plot FA, graph-1",
+    fig = plot(f, type = "graph")
   )
 
   expect_doppelganger_with_seed(
@@ -45,8 +46,8 @@ test_that("plot.see_check_model() renders correctly", {
 
   f <- parameters::factor_analysis(mtcars, n = 3, threshold = 0.5)
   expect_doppelganger_with_seed(
-    title = "plot FA, graph",
-    fig = plot(f, type = "graph-3")
+    title = "plot FA, graph-3",
+    fig = plot(f, type = "graph")
   )
 
   expect_doppelganger_with_seed(
@@ -56,5 +57,25 @@ test_that("plot.see_check_model() renders correctly", {
       type = "graph",
       colors = c("#E74C3C", "grey85", high = "#2ECC71")
     )
+  )
+
+  expect_doppelganger_with_seed(
+    title = "plot FA, graph-5",
+    fig = plot(
+      f,
+      type = "graph",
+      arrow_end_gap = 0.15,
+      factor_node_size = c(20, 45),
+      margins = c(0.1, 0.1),
+      names_factors = list(MR1 = "First", MR2 = "Second", MR3 = "Third"),
+      fill_variables = "purple",
+      fill_factors = "green"
+    )
+  )
+
+  f <- parameters::principal_components(mtcars, n = 3)
+  expect_doppelganger_with_seed(
+    title = "plot PCA, graph-1",
+    fig = plot(f, type = "graph")
   )
 })
