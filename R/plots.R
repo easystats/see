cebb0781877d17665c69d69e157d0b854776f719
Daniel <mail@danielluedecke.de>
2026-05-29 08:49:43 +0200

    err msg

--- R/plots.R
+++ R/plots.R
@@ -103,9 +103,10 @@ plots <- function(
   }
   if (inherits(pw_drawn, "simpleError")) {
     msg_display1 <- "\n- To fix this issue, please make the window larger."
-    msg_display3 <- "\n- If this still doesn't resolve your problems, you may check whether your apps are rescaled. On Windows, this can be done in the display settings (Start > Settings > System > Display, \"Scale and layout\"). Reduce the scaling and try again." # nolint
-    msg_display4 <- "\n- You can try to decrease the base font-size of your theme before plotting. Load `library(ggplot2)` and run: `theme_set(theme_classic(base_size = 6))`" # nolint
-    msg_display5 <- "\n- Finally, make sure to update your packages (in particular `ggplot2` and `patchwork`) using `update.packages(ask = FALSE)`." # nolint
+    msg_display3 <- "\n- If this still doesn't resolve your problems, you may check whether your apps are rescaled. On Windows, this can be done in the display settings (Start > Settings > System > Display, \"Scale and layout\"). Reduce the scaling and try again."
+    msg_display4 <- "\n- You can try to decrease the base font-size of your theme before plotting. Load `library(ggplot2)` and run: `theme_set(theme_classic(base_size = 6))`"
+    msg_display5 <- "\n- Make sure to update your packages (in particular `ggplot2`, `qqplotr` and `patchwork`) using `update.packages(ask = FALSE)`."
+    msg_display6 <- "\n- Finally, ensure your R version, and the IDE you use, are up to date. Running the most recent versions of R and, e.g., RStudio or Positron can resolve any remaining issues."
 
     if (Sys.getenv("RSTUDIO") == "1") {
       msg <- "The RStudio 'Plots' window is too small to show this set of plots."
@@ -125,7 +126,8 @@ plots <- function(
       msg_display2,
       msg_display3,
       msg_display4,
-      msg_display5
+      msg_display5,
+      msg_display6
     )
   }
 }
