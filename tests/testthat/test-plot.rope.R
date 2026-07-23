fec37e1f42ea50bb8945cc3730ca8412ad53eee0
Miura Meng <251798069+haomeng797-ship-it@users.noreply.github.com>
2026-07-22 22:08:27 -0400

    test: restore rstanarm tests and add rstanarm snapshots for rope and point_estimate

--- tests/testthat/test-plot.rope.R
+++ tests/testthat/test-plot.rope.R
@@ -1,4 +1,13 @@
 test_that("`plot.see_rope()` works", {
+  skip_if_not_installed("rstanarm")
+
+  set.seed(123)
+  result <- bayestestR::rope(m_rstan, verbose = FALSE)
+
+  expect_s3_class(plot(result), "gg")
+})
+
+test_that("`plot.see_rope()` snapshot", {
   x <- bayestestR::distribution_normal(1000, mean = 0.5, sd = 1)
   result <- bayestestR::rope(x, range = c(-0.1, 0.1))
   expect_s3_class(plot(result), c("gg", "ggplot"))
@@ -7,5 +16,25 @@ test_that("`plot.see_rope()` works", {
   vdiffr::expect_doppelganger(
     title = "plot.rope",
     fig = plot(result)
+  )
+})
+
+test_that("`plot.see_rope()` rstanarm snapshot", {
+  skip_on_cran()
+  skip_if_not_installed("vdiffr")
+  skip_if_not_installed("rstanarm")
+  skip_if_not_installed("curl")
+  skip_if_offline()
+  skip_if_not_installed("httr2")
+
+  set.seed(123)
+  model <- insight::download_model("stanreg_lm_1")
+  skip_if(is.null(model))
+  result <- bayestestR::rope(model, verbose = FALSE)
+
+  set.seed(123)
+  vdiffr::expect_doppelganger(
+    title = "plot.rope rstanarm",
+    fig = plot(result, data = model)
   )
 })
