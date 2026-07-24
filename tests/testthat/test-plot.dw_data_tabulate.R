823210c7573cdbc83fe0e7adc4da00c16f872df5
Miura Meng <251798069+haomeng797-ship-it@users.noreply.github.com>
2026-07-24 08:17:59 -0400

    test: add snapshot tests for cluster_analysis, data_tabulate, easycormatrix, equivalence_test and parameters_simulate plots

--- tests/testthat/test-plot.dw_data_tabulate.R
+++ tests/testthat/test-plot.dw_data_tabulate.R
@@ -7,3 +7,16 @@ test_that("`plot.datawizard_table()` works with multiple tables", {
   x <- datawizard::data_tabulate(mtcars, select = c("cyl", "carb", "am"))
   expect_true(is.list(plot(x)))
 })
+
+test_that("`plot.datawizard_table()` snapshot", {
+  skip_if_not_installed("vdiffr")
+
+  x <- datawizard::data_tabulate(mtcars, select = "cyl")
+  p <- plot(x)
+  expect_s3_class(p, c("gg", "ggplot"))
+
+  vdiffr::expect_doppelganger(
+    title = "plot.data_tabulate",
+    fig = p
+  )
+})
--- tests/testthat/test-plot.parameters_simulate.R
+++ tests/testthat/test-plot.parameters_simulate.R
@@ -4,3 +4,18 @@ test_that("`plot.see_parameters_simulate()` works", {
 
   expect_s3_class(plot(result), "gg")
 })
+
+test_that("`plot.see_parameters_simulate()` snapshot", {
+  skip_if_not_installed("vdiffr")
+
+  m <- stats::lm(mpg ~ wt + cyl + gear, data = mtcars)
+  set.seed(123)
+  result <- parameters::simulate_parameters(m)
+  p <- plot(result)
+  expect_s3_class(p, c("gg", "ggplot"))
+
+  vdiffr::expect_doppelganger(
+    title = "plot.parameters_simulate",
+    fig = p
+  )
+})
