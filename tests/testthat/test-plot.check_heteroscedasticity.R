85a55409e12a3d44fe0a4226cae88f4b4d4d864d
MiuraMeng <haomeng797@gmail.com>
2026-07-25 05:00:50 -0400

    test: add snapshot tests for check_heteroscedasticity, compare_perfor… (#457)
    Continues #236, follows #454

--- tests/testthat/test-plot.check_heteroscedasticity.R
+++ tests/testthat/test-plot.check_heteroscedasticity.R
@@ -4,3 +4,16 @@ test_that("`plot.see_check_heteroscedasticity()` works", {
 
   expect_s3_class(plot(result, data = m), "gg")
 })
+
+test_that("`plot.see_check_heteroscedasticity()` snapshot", {
+  skip_if_not_installed("vdiffr")
+
+  m <- stats::lm(mpg ~ wt + cyl + gear + disp, data = mtcars)
+  result <- performance::check_heteroscedasticity(m)
+  expect_s3_class(plot(result, data = m), c("gg", "ggplot"))
+
+  vdiffr::expect_doppelganger(
+    title = "plot.check_heteroscedasticity",
+    fig = plot(result, data = m)
+  )
+})
