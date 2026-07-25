54824cac7bcfe95b1b3493bad9b48c200b3458e0
MiuraMeng <haomeng797@gmail.com>
2026-07-25 07:29:45 -0400

    test: add snapshot tests for cluster_analysis, data_tabulate, easycor… (#458)
    Continues #236, follows #454 and #457
    
    Co-authored-by: Miura Meng <251798069+haomeng797-ship-it@users.noreply.github.com>
    Co-authored-by: Daniel <mail@danielluedecke.de>

--- tests/testthat/test-plot.easycormatrix.R
+++ tests/testthat/test-plot.easycormatrix.R
@@ -3,3 +3,17 @@ test_that("`plot.see_easycormatrix()` works", {
   s <- summary(result)
   expect_s3_class(suppressWarnings(plot(s)), "gg")
 })
+
+test_that("`plot.see_easycormatrix()` snapshot", {
+  skip_if_not_installed("vdiffr")
+
+  result <- correlation::correlation(mtcars[, -(8:9)])
+  s <- summary(result)
+  p <- suppressWarnings(plot(s))
+  expect_s3_class(p, c("gg", "ggplot"))
+
+  vdiffr::expect_doppelganger(
+    title = "plot.easycormatrix",
+    fig = p
+  )
+})
--- tests/testthat/test-plot.equivalence_test.R
+++ tests/testthat/test-plot.equivalence_test.R
@@ -4,3 +4,18 @@ test_that("`plot.see_equivalence_test()` works", {
   result_ez <- effectsize::eta_squared(m_aov, verbose = FALSE)
   expect_s3_class(plot(result_ez), "gg")
 })
+
+test_that("`plot.see_equivalence_test()` snapshot", {
+  skip_if_not_installed("vdiffr")
+  skip_if_not_installed("ggridges")
+
+  m_aov <- stats::aov(mpg ~ factor(am) * factor(cyl), data = mtcars)
+  result_ez <- effectsize::eta_squared(m_aov, verbose = FALSE)
+  p <- plot(result_ez)
+  expect_s3_class(p, c("gg", "ggplot"))
+
+  vdiffr::expect_doppelganger(
+    title = "plot.equivalence_test",
+    fig = p
+  )
+})
